2da4f299753c16b627267b96f282b8036d185376
雷胜南 <cando.i@outlook.com>
2023-06-11 17:52:31 +0800

    add converters

---
+++ src/Leisn.Xaml.Wpf/Converters/StringCompareBoolConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Leisn.Xaml.Wpf.Converters
+{
+    /// <summary>
+    /// Value compare to parameter, return bool.
+    /// Use ToString() to compare.
+    /// Use !parameter to get reversed result.
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(bool), ParameterType = typeof(string))]
+    public class StringCompareBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var vstr = value?.ToString();
+            var pstr = parameter?.ToString();
+            if (pstr?.StartsWith('!') == true)
+            {
+                pstr = pstr.Substring(1);
+                return !string.Equals(vstr, pstr);
+            }
+            return string.Equals(vstr, pstr);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Leisn.Xaml.Wpf/Converters/StringCompareVisibiltyConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Leisn.Xaml.Wpf.Converters
+{
+    /// <summary>
+    /// Value compare to parameter, return Visibility.
+    /// Use ToString() to compare.
+    /// Use !parameter to get reversed result.
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(Visibility), ParameterType = typeof(string))]
+    public class StringCompareVisibiltyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var vstr = value?.ToString();
+            var pstr = parameter?.ToString();
+            bool equals = string.Equals(vstr, pstr);
+            if (pstr?.StartsWith('!') == true)
+            {
+                pstr = pstr.Substring(1);
+                equals = !string.Equals(vstr, pstr);
+            }
+            return equals ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
