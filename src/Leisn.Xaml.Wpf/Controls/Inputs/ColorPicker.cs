c07fc49536251ee7a23fe0da0c7c72961de13e2c
雷胜南 <cando.i@outlook.com>
2023-06-13 18:46:45 +0800

    nothing

---
+++ src/Leisn.Xaml.Wpf/Controls/Inputs/ColorPicker.cs
@@ -0,0 +1,20 @@
+﻿// @Leisn (https://leisn.com , https://github.com/leisn)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Leisn.Xaml.Wpf.Controls
+{
+    public class ColorPicker : Control
+    {
+        static ColorPicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+        }
+    }
+}
