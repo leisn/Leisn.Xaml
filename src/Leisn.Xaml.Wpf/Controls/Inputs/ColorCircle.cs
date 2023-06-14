8c8388019fac4767d0d11f5bb56eb557ae256833
雷胜南 <leishengnan@megarobo.tech>
2023-06-15 00:34:34 +0800

    Update ColorCircle.cs

--- src/Leisn.Xaml.Wpf/Controls/Inputs/ColorCircle.cs
+++ src/Leisn.Xaml.Wpf/Controls/Inputs/ColorCircle.cs
@@ -57,13 +57,15 @@ namespace Leisn.Xaml.Wpf.Controls
         static readonly SKColor[] _colors = new SKColor[]
         {
             new SKColor(255, 0, 0),
-            new SKColor(255, 0, 255),
-            new SKColor(0, 0, 255),
-            new SKColor(0, 255, 255),
-            new SKColor(0, 255, 0),
             new SKColor(255, 255, 0),
-            new SKColor(255, 0, 0),
+            new SKColor(0, 255, 0),
+            new SKColor(0, 255, 255),
+            new SKColor(0, 0, 255),
+            new SKColor(255, 0, 255),
+            new SKColor(255, 0, 0)
         };
+        static readonly float[] _colorPos = new float[] { 0, 0.167f, 0.334f, 0.501f, 0.668f, 0.835f, 1 };
+
         protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
         {
             var canvas = e.Surface.Canvas;
@@ -75,126 +77,25 @@ namespace Leisn.Xaml.Wpf.Controls
             float innerRadius = outerRadius - circleLength;
             canvas.Save();
             canvas.Translate(center);
-
+            canvas.RotateDegrees(-90);
             using var paint = new SKPaint
             {
                 IsStroke = false,
                 IsAntialias = true,
             };
             //draw colors
-            #region draw 1
-            float areaAngle = 360f / (_colors.Length - 1) / 256;
-            float areaRadians = (float)(Math.PI * 2 / (_colors.Length - 1) / 256);
-            var areaRotation = Matrix3x2.CreateRotation(areaRadians);
-            Vector2 point1 = Vector2.Transform(new Vector2(innerRadius, 0), areaRotation);
-            Vector2 point2 = Vector2.Transform(new Vector2(outerRadius, 0), areaRotation);
-            SKColor color;
-            //for (int i = 0; i < _colors.Length - 1; i++)
-            //{
-            //    color = _colors[i];
-            //    byte j = 0;
-            //    while (true)
-            //    {
-            //        var val = i % 2 == 0 ? j : (byte)(255 - j);
-            //        switch (i)
-            //        {
-            //            case 0:
-            //                color = color.WithBlue(val);
-            //                break;
-            //            case 1:
-            //                color = color.WithRed(val);
-            //                break;
-            //            case 2:
-            //                color = color.WithGreen(val);
-            //                break;
-            //            case 3:
-            //                color = color.WithBlue(val);
-            //                break;
-            //            case 4:
-            //                color = color.WithRed(j);
-            //                break;
-            //            case 5:
-            //                color = color.WithGreen(val);
-            //                break;
-            //        }
-            //        paint.Color = color;
-
-            //        using (var path = new SKPath())
-            //        {
-            //            path.MoveTo(innerRadius, 0);
-            //            path.ArcTo(innerRadius, innerRadius, areaAngle, SKPathArcSize.Small, SKPathDirection.Clockwise, point1.X, point1.Y);
-            //            path.LineTo(point2.X, point2.Y);
-            //            path.ArcTo(outerRadius, outerRadius, areaAngle, SKPathArcSize.Small, SKPathDirection.CounterClockwise, outerRadius, 0);
-            //            path.LineTo(innerRadius, 0);
-            //            path.Close();
-            //            canvas.DrawPath(path, paint);
-            //        }
-            //        canvas.RotateDegrees(areaAngle, 0, 0);
-            //        if (j == 255)
-            //            break;
-            //        j++;
-            //    }
-            //}
-            #endregion
-
-            #region draw2
-            for (int i = 0; i < _colors.Length - 1; i++)
-            {
-                color = _colors[i];
-                byte j = 0;
-                while (true)
-                {
-                    var val = i % 2 == 0 ? j : (byte)(255 - j);
-                    switch (i)
-                    {
-                        case 0:
-                            color = color.WithBlue(val);
-                            break;
-                        case 1:
-                            color = color.WithRed(val);
-                            break;
-                        case 2:
-                            color = color.WithGreen(val);
-                            break;
-                        case 3:
-                            color = color.WithBlue(val);
-                            break;
-                        case 4:
-                            color = color.WithRed(j);
-                            break;
-                        case 5:
-                            color = color.WithGreen(val);
-                            break;
-                    }
-                    paint.Color = color;
-
-                    using (var path = new SKPath())
-                    {
-                        path.MoveTo(0, 0);
-                        path.LineTo(outerRadius, 0);
-                        path.ArcTo(outerRadius, outerRadius, areaAngle, SKPathArcSize.Small, SKPathDirection.Clockwise, point2.X, point2.Y);
-                        path.LineTo(0, 0);
-                        path.Close();
-                        canvas.DrawPath(path, paint);
-                    }
-                    canvas.RotateDegrees(areaAngle, 0, 0);
-                    if (j == 255)
-                        break;
-                    j++;
-                }
-            }
-
+            #region draw
+            using (var shade = SKShader.CreateSweepGradient(new SKPoint(), _colors, _colorPos))
             using (var clip = new SKPath { FillType = SKPathFillType.EvenOdd })
             {
                 clip.AddCircle(0, 0, innerRadius);
                 clip.AddCircle(0, 0, outerRadius);
-                canvas.ClipPath(clip, SKClipOperation.Difference, true);
-                paint.Color = SKColors.White;
+                canvas.ClipPath(clip, SKClipOperation.Intersect, true);
+                paint.Shader = shade;
                 canvas.DrawPaint(paint);
             }
 
             #endregion
-
 
             //draw broder
             if (BorderWidth > 0)
