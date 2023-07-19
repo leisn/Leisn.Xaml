428f23ad26414c8734d7a3956d6d61c17933c4bd
leisn <cando.i@outlook.com>
2023-07-19 14:30:53 +0800

    Create ICloneable{T}.cs

---
+++ src/Leisn.Common/ICloneable{T}.cs
@@ -0,0 +1,17 @@
+﻿// @Leisn (https://leisn.com , https://github.com/leisn)
+
+using System;
+
+namespace Leisn.Common
+{
+    /// <inheritdoc/>
+    public interface ICloneable<T> : ICloneable
+    {
+        /// <summary>
+        /// Creates a new {T} that is a copy of the current instance.
+        /// </summary>
+        /// <returns> A new {T} that is a copy of this instance.</returns>
+        new T Clone();
+        object ICloneable.Clone() => Clone()!;
+    }
+}
