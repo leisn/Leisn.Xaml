430b71a7b68ddced98be4dfd35364429fe0bfb3f
leisn <cando.i@outlook.com>
2023-07-23 18:23:05 +0800

    datetimepicker 2

--- src/Leisn.Xaml.Wpf/Controls/Inputs/DateTimes/DateTimePicker.cs
+++ src/Leisn.Xaml.Wpf/Controls/Inputs/DateTimes/DateTimePicker.cs
@@ -2,36 +2,17 @@
 
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 using SkiaSharp.Views.WPF;
 
 namespace Leisn.Xaml.Wpf.Controls
 {
-    [TemplatePart(Name = PART_CanvasName, Type = typeof(SKElement))]
     public class DateTimePicker : Control
     {
-        const string PART_CanvasName = "PART_Canvas";
-        private SKElement _canvas = null!;
         static DateTimePicker()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DateTimePicker), new FrameworkPropertyMetadata(typeof(DateTimePicker)));
-        }
-
-
-        public override void OnApplyTemplate()
-        {
-            if (_canvas != null)
-            {
-                _canvas.PaintSurface -= OnPaintCanvas;
-            }
-            base.OnApplyTemplate();
-            _canvas = (SKElement)GetTemplateChild(PART_CanvasName);
-            _canvas.PaintSurface += OnPaintCanvas;
-        }
-
-        private void OnPaintCanvas(object? sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
